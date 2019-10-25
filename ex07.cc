7c72255345849b07467c5cc6e6cda4d426f1df94
kef012 <nukokefu@gmail.com>
2019-10-25 13:40:00 +0900

    is appeared <- strange

--- ex07.cc
+++ ex07.cc
@@ -5,7 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-using namespace std;
+
 
 bool space(char c){
     return (isspace(c));
@@ -15,14 +15,13 @@ bool not_space(char c){
     return (!isspace(c));
 }
 
-vector<string> split(string str){
-    vector<string> words;
-    string::iterator i,j;
+std::vector<std::string> split(std::string str){
+    std::vector<std::string> words;
+    std::string::iterator i,j;
     
     i = str.begin();
     j = str.begin();
-    //cout << (*str.end()) << endl;
-    
+
     while(j+2 != str.end()){ // j+2 space 
         if(i != str.begin() ) j = ++i;
 
@@ -33,77 +32,111 @@ vector<string> split(string str){
         i = find_if(i, str.end(), not_space);
         j = find_if(j, str.end(), space);
 
-        if ( isspace(*j)){ //debug
-            cout << "(*j) is space!" << endl;
-        }
-
-        string s(i, j);
+        std::string s(i, j);
         if ( isspace(s.size()-1)){ 
-            cout << "space!" << endl;
+            std::cout << "space!" << std::endl;
         }
         
         i = i+s.size();
         j = i;
       
-        cout << "push_back " << s << endl;
+        //std::cout << "push_back " << s << std::endl;
     
         words.push_back(s); 
 
-        if(j+2 == str.end()){cout << "finish" << endl;}
+        //if(j+2 == str.end()){std::cout << "finish" << std::endl;}
     }
-    cout << "bbb" << endl;
     return words;
 }
 
 int main(){
 
-    map<string, vector<int> > counters;
+    std::map<std::string, std::vector<int> > counters;
     
-    vector<string> str;
-    string line;
+    std::vector<std::string> str;
+    std::string line;
     int lnum = 0; //行番号
-    string q; //探す単語
+    std::string q; //探す単語
 
-    while(  getline(cin, line) ){
-        cout << "get line"<< endl;
-        cout << line << endl;
-        cout << "------" << endl;
+    while(  getline(std::cin, line) ){
+        std::cout << "obtained line is"<< std::endl;
+        std::cout << line << std::endl;
+        std::cout << std::string(10,'-') << std::endl;
         ++lnum;
         
-        if(lnum == 1 ) q = line;
-        if(line == ""){ cout << "nothing"  << endl;}
+        if(lnum == 1 ){
+            q = line;
+            std::cout << "let's search " << q << std::endl;
+        }
+
+        if(line == ""){ std::cout << "nothing"  << std::endl;}
         if(lnum != 1 && line.size() != 0){
             str = split(line); 
-           // copy( , , back_inserter(str));
-            cout << "ccc" << endl;
 
-            vector<string>::iterator iter = str.begin();
-            while(iter != str.end()){ // about a word on the line.
-                map<string, vector<int> >::iterator mapiter = counters.begin();
-                while(mapiter != counters.end()){ // about counters
-                    if( mapiter->first != (*iter) ){ //もし単語が無いなら登録する
-                        counters[(*iter)]; //counters に登録
-                        cout << (*iter) << "　is registered" << endl;
+        // strに記録された文字について整形を行う
+        std::vector<std::string>::iterator striter;
+        for(striter = str.begin(); striter < str.end(); striter++){
+            //std::cout << "seikei" << std::endl;
+            if((*striter).substr(0,1) == "(" ||
+               (*striter).substr(0,1) == "\'" ||
+               (*striter).substr(0,1) == "\""){
+               (*striter).erase(0,1);
+            }
+            if((*striter).substr((*striter).size()-1,(*striter).size()) == ")" ||
+               (*striter).substr((*striter).size()-1,(*striter).size()) == "\'" ||
+               (*striter).substr((*striter).size()-1,(*striter).size()) == "?" ||
+               (*striter).substr((*striter).size()-1,(*striter).size()) == ":" ||
+               (*striter).substr((*striter).size()-1,(*striter).size()) == "\""){
+               (*striter).pop_back();
+            }
+            if((*striter).substr((*striter).size()-1,(*striter).size()) == "," || 
+               (*striter).substr((*striter).size()-1,(*striter).size()) == "." ||
+               (*striter).substr((*striter).size()-1,(*striter).size()) == "!") {
+               (*striter).pop_back();
+            }
+        }
+        transform ((*striter).begin(), (*striter).end(), (*striter).begin(), tolower);
+        // 整形ここまで
+           // std::cout << "ccc" << std::endl;
+
+            striter = str.begin();
+
+            while(striter != str.end()){ // about a word on the line.
+                std::map<std::string, std::vector<int> >::iterator mapiter = counters.begin();
+                int isFound = 0;
+                while(mapiter != counters.end()){ // search all of counters  
+                    if( mapiter->first == (*striter) ){ //もし単語があったらisFoundを1増やす
+                        isFound = 1;
                     }
-                     ++mapiter;
-                }    
-                counters[(*iter)].push_back(lnum);
-                ++iter;
+                    ++mapiter;
+                }
+
+                if(isFound != 1){
+                    counters[(*striter)]; //counters に登録
+                  //  std::cout << (*striter) << "　is registered" << std::endl;
+                }
+                counters[(*striter)].push_back(lnum);
+                ++striter;
             }
             str.clear();
         } 
     }
-    map<string, vector<int> >::iterator ite = counters.begin();
-    vector<int>::iterator linenumiterator = ite->second.begin();
-    cout << "first " << ite->first << " "<<(*linenumiterator) << endl;
+    /*
+    std::map<std::string, std::vector<int> >::iterator ite = counters.begin();
+    std::vector<int>::iterator linenumiterator = ite->second.begin();
+    std::cout << "first " << ite->first << " "<<(*linenumiterator) << std::endl;
+    */
    // map< string, vector<int> >::iterator iter = counters.begin();
-    vector<int>::iterator iter = counters[q].begin();
-    cout << q <<  " is appeared "; // c++ is appeared
-    while(iter != counters[q].end()){ 
-        cout << (*iter) << "\t"; // line
-        ++iter;
+
+    std::cout << "RESULT for " << q << std::endl;
+    std::cout << ">>>>>" << q << " is appeared " << std::endl; // c++ is appeared
+    std::vector<int>::iterator linenumiter = counters[q].begin();
+    
+    while(linenumiter != counters[q].end()){ 
+        std::cout << (*linenumiter) << "\t"; // line
+        ++linenumiter;
     }
-    cout << endl;
+    std::cout << std::endl;
 
     return 0;
 
