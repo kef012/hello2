38429394fd3d11dc2323ef419a09d671c5c75277
kef012 <nukokefu@gmail.com>
2019-10-25 16:34:19 +0900

    何かがおかしい

--- ex07.cc
+++ ex07.cc
@@ -5,6 +5,8 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <cctype>
+#include <cstdlib>
 
 
 bool space(char c){
@@ -25,7 +27,7 @@ std::vector<std::string> split(std::string str){
     while(j+2 != str.end()){ // j+2 space 
         if(i != str.begin() ) j = ++i;
 
-        if( space((*i)) ){ 
+        if( space((*i)) ){ //段落があった場合二文字進める
             i = i+2;
             j = i;
         }
@@ -33,9 +35,12 @@ std::vector<std::string> split(std::string str){
         j = find_if(j, str.end(), space);
 
         std::string s(i, j);
+        /*
         if ( isspace(s.size()-1)){ 
-            std::cout << "space!" << std::endl;
+            std::cout << s << " has space!" << std::endl;
+        //    std::string s(i, j-1);
         }
+        */
         
         i = i+s.size();
         j = i;
@@ -56,51 +61,58 @@ int main(){
     std::vector<std::string> str;
     std::string line;
     int lnum = 0; //行番号
-    std::string q; //探す単語
+    std::string q = ""; //探す単語
 
     while(  getline(std::cin, line) ){
+        /*
         std::cout << "obtained line is"<< std::endl;
         std::cout << line << std::endl;
         std::cout << std::string(10,'-') << std::endl;
+        */
         ++lnum;
+        transform(line.begin(), line.end(), line.begin(), tolower);
         
         if(lnum == 1 ){
-            q = line;
-            std::cout << "let's search " << q << std::endl;
+            counters[line];
+            q = (*counters.begin()).first;
+            std::cout << "let's search " << (*counters.begin()).first << std::endl;
         }
 
         if(line == ""){ std::cout << "nothing"  << std::endl;}
-        if(lnum != 1 && line.size() != 0){
-            str = split(line); 
 
-        // strに記録された文字について整形を行う
-        std::vector<std::string>::iterator striter;
-        for(striter = str.begin(); striter < str.end(); striter++){
-            //std::cout << "seikei" << std::endl;
-            if((*striter).substr(0,1) == "(" ||
-               (*striter).substr(0,1) == "\'" ||
-               (*striter).substr(0,1) == "\""){
-               (*striter).erase(0,1);
+        if(lnum != 1 && line.size() != 0){ 
+            str = split(line); 
+            // strに記録された文字について整形を行う
+            std::vector<std::string>::iterator striter;
+            
+            for(striter = str.begin(); striter < str.end(); striter++){
+                //std::cout << "before : " << (*striter) << std::endl;
+                if((*striter).substr(0,1) == "(" ||
+                   (*striter).substr(0,1) == "\'" ||
+                   (*striter).substr(0,1) == "\""){
+                   (*striter).erase(0,1);
+                }
+                if((*striter).substr((*striter).size()-1,(*striter).size()) == ")" ||
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == "\'" ||
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == "\""){
+                   (*striter).pop_back();
+                }
+                if((*striter).substr((*striter).size()-1,(*striter).size()) == "," || 
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == "." ||
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == ":" ||
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == "?" ||
+                   (*striter).substr((*striter).size()-1,(*striter).size()) == "!") {
+                   (*striter).pop_back();
+                }
             }
-            if((*striter).substr((*striter).size()-1,(*striter).size()) == ")" ||
-               (*striter).substr((*striter).size()-1,(*striter).size()) == "\'" ||
-               (*striter).substr((*striter).size()-1,(*striter).size()) == "?" ||
-               (*striter).substr((*striter).size()-1,(*striter).size()) == ":" ||
-               (*striter).substr((*striter).size()-1,(*striter).size()) == "\""){
-               (*striter).pop_back();
+            /*
+            for(striter = str.begin(); striter < str.end(); striter++){
+                std::cout << "after : " << (*striter) << std::endl;
             }
-            if((*striter).substr((*striter).size()-1,(*striter).size()) == "," || 
-               (*striter).substr((*striter).size()-1,(*striter).size()) == "." ||
-               (*striter).substr((*striter).size()-1,(*striter).size()) == "!") {
-               (*striter).pop_back();
-            }
-        }
-        transform ((*striter).begin(), (*striter).end(), (*striter).begin(), tolower);
-        // 整形ここまで
-           // std::cout << "ccc" << std::endl;
+            */
+            // 整形ここまで
 
             striter = str.begin();
-
             while(striter != str.end()){ // about a word on the line.
                 std::map<std::string, std::vector<int> >::iterator mapiter = counters.begin();
                 int isFound = 0;
@@ -113,7 +125,7 @@ int main(){
 
                 if(isFound != 1){
                     counters[(*striter)]; //counters に登録
-                  //  std::cout << (*striter) << "　is registered" << std::endl;
+                    //std::cout << (*striter) << " is registered" << std::endl;
                 }
                 counters[(*striter)].push_back(lnum);
                 ++striter;
@@ -121,29 +133,41 @@ int main(){
             str.clear();
         } 
     }
-    /*
-    std::map<std::string, std::vector<int> >::iterator ite = counters.begin();
-    std::vector<int>::iterator linenumiterator = ite->second.begin();
-    std::cout << "first " << ite->first << " "<<(*linenumiterator) << std::endl;
-    */
-   // map< string, vector<int> >::iterator iter = counters.begin();
-
-    //q = "c++"
-    std::cout << q << " is appeared " << std::endl;
-    //何故か出力されない　is appeared だけ出てくる
-    std::cout << "RESULT for " << q << std::endl;
-    //この行は正しく表示される RESULT for c++
-    std::cout << "aaaaa " << q << " is appeared " << q <<"is appeared" << std::endl; // c++ is appeared
-    //なぜか aaaaa と qの中身が表示されない 
     
+    //検索結果出力
+    std::string query = q;
+    std::cout << "let's search " << q <<"!"<< std::endl;
+    // let's search c++! って出力されるはず！
+    std::cout << "なんでや" << std::endl;
+    std::map<std::string, std::vector<int> >::iterator mapiter;
+    mapiter = counters.begin();
+    while(mapiter != counters.end()){
+       
+            std::cout << mapiter->first << " is appeared at ";
+            std::vector<int>::iterator linenumiter = counters[mapiter->first].begin();
 
+        while(linenumiter != counters[mapiter->first].end()){ 
+            std::cout << (*linenumiter) << " "; // line
+            ++linenumiter;
+        }
+        std::cout << std::endl;
+         if(mapiter->first == query){
+            /*
+            std::cout << "RESULT " ; 
+            std::cout << mapiter->first << " is appeared " << std::endl;
+            */
+            break;
+        }
+       ++mapiter;
+    }
+    /*
     std::vector<int>::iterator linenumiter = counters[q].begin();
     while(linenumiter != counters[q].end()){ 
         std::cout << (*linenumiter) << "\t"; // line
         ++linenumiter;
     }
     std::cout << std::endl;
-
+    */
     return 0;
 
 }
